a15b8f3cd93c57a2731765976470524f51a20da1
erganeshsharma <ganeshsharmait@gmail.com>
2017-07-07 11:31:23 +0530

    Changing the voting options

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Futbol"
-optionB = "Soccer"
+optionA = "Cricket"
+optionB = "Badminton"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
